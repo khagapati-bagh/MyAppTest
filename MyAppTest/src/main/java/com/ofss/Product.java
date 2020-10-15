5a4cd25296ce21d2348c5589c7333f632b6eeff0
khagapati-bagh <mr.k.affinity@gmail.com>
2020-10-15 15:00:59 +0530

    Added main class

--- MyAppTest/src/main/java/com/ofss/Product.java
+++ MyAppTest/src/main/java/com/ofss/Product.java
@@ -3,4 +3,31 @@ package com.ofss;
 public class Product {
 	private String pName;
 	private String pId;
+	
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getpId() {
+		return pId;
+	}
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+	public Product(String pName, String pId) {
+		super();
+		this.pName = pName;
+		this.pId = pId;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Product [pName=" + pName + ", pId=" + pId + "]";
+	}
+	
 }
---
+++ MyAppTest/src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,12 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product product = new Product("Phone", "Iphone1");
+		System.out.println(product);
+
+	}
+
+}
